d6bbf96fc093eb7fe0359b4f3e47796ed2590090
PV <p_vilpola@yahoo.com>
2023-07-31 15:44:50 +0300

    Added rest of Osa2 tasks

--- osa2/puhelinluettelo/App.js
+++ osa2/puhelinluettelo/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import './index.css';
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const Filter = (props) => {
   const value = props.value
@@ -42,7 +43,9 @@ const PersonForm = (props) => {
 }
 
 const Persons = (props) => {
+  console.log('now on Persons method, delete method is ', props.deletePerson)
   const personsToShow = props.personsToShow
+  const deletePerson = props.deletePerson
 
   if (personsToShow.length === 0){
     return (<p>No persons found matching the search filter</p>)
@@ -51,25 +54,53 @@ const Persons = (props) => {
   return (
        <ul>
           {personsToShow.map(person => 
-            <li key={person.name}>{person.name} {person.number}</li>)}
+            <li key={person.name}>{person.name} {person.number} 
+              <button onClick={() => deletePerson(person.id)}>delete</button>
+            </li>)}
        </ul>
+  )
+}
+
+const Notification = ({ message, isError }) => {
+  if (message === null) {
+    return null
+  }
+
+  if (isError){
+    isError = false
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  }
+
+  return (
+    <div className="notification">
+      {message}
+    </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([])
+  const [personToDelete, setPersonToDelete] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameSearch, setNameSearch] = useState('')
+  const [notification, setNotification] = useState(null)
+  const [isError, setIsError] = useState('false')
 
+  //read persons data from server
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
+    personService
+      .getAll()
+        .then(initialPersons => {        
+          setPersons(initialPersons)      
+        })
+        .catch(error => {
+          alert('failed to read initialization data')
+        })
   }, [])
 
   const personsToShow = nameSearch === "" ? persons : 
@@ -80,13 +111,76 @@ const App = () => {
 
     //if there is to be found given name already
     if (persons.map(person => person.name).includes(newName)){
-      alert(`${newName} is already added to phonebook`)
+
+      if (!window.confirm(`${newName} is already added to phonebook. Do you want to replace number of the original?`)){
+        setNotification(`${newName} is already added to phonebook.`)
+        setIsError(true)
+        return
+      }
+
+      const personToUpdate = persons.find(p => p.name === newName)
+      setNotification(`Updated number info of ${newName}`)
+      setIsError(false)
+
+      personService
+        .update(personToUpdate.id,{ name: newName, number: newNumber})
+          .then(returnedPerson => {  
+            setPersons(persons.filter(p => p.name !== newName).concat(returnedPerson))  
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(error => {
+            setIsError(true)
+            setNotification('failed to add new person info to server')
+          })
+
       return
     }
 
-    setPersons(persons.concat({ name: newName, number: newNumber}))
-    setNewName('')
-    setNewNumber('')
+    setNotification(`Added ${newName}`)
+    setIsError(false)
+
+    //add new person info to server
+    personService
+      .create({ name: newName, number: newNumber})
+        .then(returnedPerson => {        
+          setPersons(persons.concat(returnedPerson))        
+          setNewName('')
+          setNewNumber('')
+        })
+        .catch(error => {
+          setIsError(true)
+          setNotification('failed to add new person info to server')
+        })
+  }
+
+  // delete person info from server
+  const deletePerson = (persontodelete) =>{
+    const personToDeleteName = persons.find(p => p.id === persontodelete).name
+    console.log('person to delete info: ', personToDeleteName, persontodelete)
+
+    if (!window.confirm(`Do you really want to remove person ${personToDeleteName}`)){
+      setNotification(null)
+      console.log(`Deletion of ${personToDeleteName} canceled`)
+      return
+    }
+
+    console.log('Now will try to delete person ', personToDeleteName)
+    setPersonToDelete(persontodelete)
+    setNotification(`Deleted ${personToDeleteName}`)
+    setIsError(false)
+
+    personService
+      .deletedata(personToDelete)
+        .then(response => {
+          console.log(response)
+          setPersons(persons.filter(p => p.id !== personToDelete))
+          setPersonToDelete('')
+        })
+        .catch(error => {
+          setIsError(true)
+          setNotification('failed to delete person info to server')
+        })
   }
   
   const handleNameChange = (event) => {
@@ -104,13 +198,12 @@ const App = () => {
     setNameSearch(event.target.value)
   }
 
-  console.log('Shown persons are ', personsToShow)
-
   return (
     <div>
       <h2>Phonebook</h2>
         <Filter value = {nameSearch} onChange = {handleNameSearchChange} />
       <h2>add a name</h2>
+        <Notification message={notification} isError={isError}/>
         <PersonForm 
           addName = {addName}
           newName = {newName}
@@ -119,7 +212,7 @@ const App = () => {
           handleNumberChange = {handleNumberChange}
         />
       <h2>Numbers</h2>
-        <Persons personsToShow={personsToShow} />
+        <Persons personsToShow={personsToShow} deletePerson={deletePerson} />
     </div>
   )
 }
